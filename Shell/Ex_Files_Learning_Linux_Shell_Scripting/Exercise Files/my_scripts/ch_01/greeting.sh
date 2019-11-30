fc08d99c6d84e519be72022075075cb04d2714b6
edwardreyes29 <reyes.edwardf@gmail.com>
2019-11-29 22:17:22 -0800

    latest commit

--- Shell/Ex_Files_Learning_Linux_Shell_Scripting/Exercise Files/my_scripts/ch_01/greeting.sh
+++ Shell/Ex_Files_Learning_Linux_Shell_Scripting/Exercise Files/my_scripts/ch_01/greeting.sh
@@ -1,6 +1,7 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
-FIRST_NAME=Edward
+# FIRST_NAME=Edward
+NAME="Edward Reyes"
 FAVORITE_COLOR=Red
 
-echo Hi $FIRST_NAME, your favorite color is $FAVORITE_COLOR
+echo Hi $NAME, your favorite color is $FAVORITE_COLOR
