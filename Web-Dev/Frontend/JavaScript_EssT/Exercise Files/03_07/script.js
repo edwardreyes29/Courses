9044efca7466c057a2ec32de4253d1c3ab0e91aa
edwardreyes29 <reyes.edwardf@gmail.com>
2020-01-30 21:07:24 -0800

    ch 3

---
+++ Web-Dev/Frontend/JavaScript_EssT/Exercise Files/03_07/script.js
@@ -0,0 +1,12 @@
+var pens;
+pens = ["red", "blue", "green", "orange"];
+
+// Arrays can also contain elements of different types
+var mixedBag = ["keys", 5, false, 568, "one more thing"];
+console.log(pens);
+console.log(mixedBag);
+
+pens[3] = "purple";
+var fourthPen = pens[3];
+console.log(fourthPen);
+
