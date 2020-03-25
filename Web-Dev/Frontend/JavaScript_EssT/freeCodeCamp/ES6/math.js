af8e4aeeae72cb6a9ebbcb8f1b352a54101894be
Edward Reyes <reyes.edwardf@gmail.com>
2020-03-24 23:57:00 -0700

    es6 75% complete

---
+++ Web-Dev/Frontend/JavaScript_EssT/freeCodeCamp/ES6/math.js
@@ -0,0 +1,19 @@
+const add =  (x,y) => {
+    return x + y;
+}
+
+const subtract = (x,y) => {
+    return x - y;
+}
+
+const multiply = (x,y) => {
+    return x * y;
+}
+
+const divide = (x,y) => {
+    return x / y;
+}
+
+export {add, subtract, multiply, divide};
+
+console.log("Hi there from math.js");
