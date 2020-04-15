c3c088b5164d8604b93f53e53530372ca4a0a5ca
Edward Reyes <reyes.edwardf@gmail.com>
2020-04-14 23:24:33 -0700

    new commit lp

---
+++ Python/Learning_Python/MyFiles/variables.py
@@ -0,0 +1,10 @@
+# f = 0
+# print(f)
+# f = "abc"
+# print("this is a string " + f) # causes error
+# print("this is a string " + str(123))
+
+# Global vs. local variables in functions
+def someFunction():
+    f = "def"
+    print(f)
