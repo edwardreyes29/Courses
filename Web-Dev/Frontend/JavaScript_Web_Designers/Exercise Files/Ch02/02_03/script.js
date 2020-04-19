6067f905683b52abc14d125b784485ec27971a4a
Edward Reyes <reyes.edwardf@gmail.com>
2020-04-18 17:42:51 -0700

    js web 03

---
+++ Web-Dev/Frontend/JavaScript_Web_Designers/Exercise Files/Ch02/02_03/script.js
@@ -0,0 +1,13 @@
+/* Self-Invoking Functions:
+    Is invoked automatically without being called.
+*/
+(function() {
+    "use strict";
+
+    // Your code goes here.
+    console.log("Hello");
+    console.error("OH NO");
+
+    var variable1 = 2, variable2 = "Hello";
+    console.log("Variable 1, and variable 2:", variable1, variable2);
+})();
---
+++ Web-Dev/Frontend/JavaScript_Web_Designers/MyFile/ch2/script.js
@@ -0,0 +1,6 @@
+(function() {
+    "use strict";
+
+    console.log("Hello");
+    console.error("OH NO");
+})();
